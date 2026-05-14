662c10a9f4a9eb77bd751fd9f62d00f54dbb179b
oleksii <zaloznyy2006@gmail.com>
2026-05-14 15:40:40 +0200

    Add ICurrencyService

---
+++ OrderFlow/OrderFlow/services/ICurrencyService.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderFlow.Services;
+
+public interface ICurrencyService
+{
+    Task<decimal?> GetRateAsync(string currencyCode);
+
+    Task<decimal> ConvertAsync(
+        decimal amount,
+        string fromCurrency,
+        string toCurrency);
+}
